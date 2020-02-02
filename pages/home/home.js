2133712862c081d0097c982966397bdf08976586
CrabMen <tobecrabman@163.com>
2020-02-02 21:25:54 +0800

    首页界面基础以及getLocation使用

--- pages/home/home.js
+++ pages/home/home.js
@@ -12,6 +12,15 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.getLocation({
+      type: 'wgs84',
+      altitude: false,
+      success: (result)=>{
+        console.log(result)
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
 
   },
 
